2890cedf5262fe7efb67ba4afea6713ff0cfc8bc
Topher Dunlap <thelonioustopher@yahoo.com>
2021-07-08 10:22:10 -0500

    error handeling updates

--- src/main/java/learn/field_agent/controllers/GlobalExceptionHandler.java
+++ src/main/java/learn/field_agent/controllers/GlobalExceptionHandler.java
@@ -3,11 +3,23 @@ package learn.field_agent.controllers;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleException(HttpMessageNotReadableException ex) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleException(HttpMediaTypeNotSupportedException ex) {
+        return new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponse> handleException(DataIntegrityViolationException ex) {
